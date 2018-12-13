1bea2c674165622586666f54e41e26972b29cce4
Arham4 <arhamjsiddiqui@gmail.com>
2018-12-13 10:08:29 -0600

    visual components of the board

--- Concentration/BoardController.swift
+++ Concentration/BoardController.swift
@@ -22,17 +22,17 @@ class BoardController: UIViewController {
     }
     
     private func constructCards() {
-        for _ in 1...4 {
+        for _ in 1...3 {
             let row = UIStackView()
             rowStackView.addArrangedSubview(row)
             row.axis = NSLayoutConstraint.Axis.horizontal
             row.distribution = UIStackView.Distribution.fillEqually
-            row.spacing = 2
+            row.spacing = 4
             row.translatesAutoresizingMaskIntoConstraints = false
             row.widthAnchor.constraint(equalTo: rowStackView.widthAnchor).isActive = true
             row.trailingAnchor.constraint(equalTo: rowStackView.trailingAnchor).isActive = true
             row.leadingAnchor.constraint(equalTo: rowStackView.leadingAnchor).isActive = true
-            for _ in 1...3 {
+            for _ in 1...4 {
                 let view = UIView()
                 row.addArrangedSubview(view)
                 view.translatesAutoresizingMaskIntoConstraints = false
@@ -50,25 +50,26 @@ class BoardController: UIViewController {
                 cardStackView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
                 cardStackView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
                 
-                changeViewBackground(view, imageName: "blank_card")
+                changeViewBackground(view, imageName: "blankCard")
                 
                 let card = Card(animal: Animal.giraffe)
                 let attributeImage = findCardImage(card: card)
                 
                 let imageView = UIImageView()
-                imageView.contentMode = UIView.ContentMode.center
+                imageView.contentMode = UIView.ContentMode.scaleAspectFit
                 imageView.translatesAutoresizingMaskIntoConstraints = false
                 cardStackView.addArrangedSubview(imageView)
                 imageView.image = attributeImage
                 imageView.widthAnchor.constraint(equalTo: cardStackView.widthAnchor, constant: 0).isActive = true
-                
+            
             }
 
         }
     }
     
     private func changeViewBackground(_ view: UIView, imageName: String) {
-        
+        let backgroundImage = UIImage(named: imageName)
+        view.layer.contents = backgroundImage?.cgImage
     }
     
     private func findCardImage(card: Card) -> UIImage {
